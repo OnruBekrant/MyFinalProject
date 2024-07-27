cddb259e48c3513355253f21ee4f387fb655ecd5
Onru Bekrant <cerimgt@gmail.com>
2024-07-27 18:48:50 +0300

    08:00.38.38

--- Entities/Concrete/Category.cs
+++ Entities/Concrete/Category.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 namespace Entities.Concrete
 {
     //çıplak class kalmasın
-    internal class Category : IEntity
+    public class Category : IEntity
     {
         public int CategoryId { get; set; }
         public string CategoryName { get; set; }
